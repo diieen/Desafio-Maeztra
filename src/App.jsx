dc62963a90eb2d742c775fa4456595a6135fdd86
Jean Ferreira <jeaferr22@gmail.com>
2022-10-06 03:21:22 -0300

    fix: erros netlify

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+const App = () => {
+    return (
+        <div className="store__template">
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
+import App from './App'
+
 import "./styles/main.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <Home />
-    <Footer />
+    <App />
   </React.StrictMode>
 )
