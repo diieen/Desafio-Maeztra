06729cdd8cc98e0c499812bf4413ee72c21bade2
Jean Ferreira <jeaferr22@gmail.com>
2022-10-06 03:05:22 -0300

    fix: change name import footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/header'
-import Footer from './components/Footer'
+import Footer from './components/footer'
 import Home from './pages/Home'
 import "./styles/main.scss"
 
