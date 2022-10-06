f5dd8d8ad3eebf2e4f529044b260405211c8dbce
Jean Ferreira <jeaferr22@gmail.com>
2022-10-06 03:14:07 -0300

    fix: netlify errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header/index'
+import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import "./styles/main.scss"
